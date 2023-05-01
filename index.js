47a5bd2fc11b65d9ab9439d5f2a59a500254e9aa
Bob <mike.baradaran@qa.com>
2023-05-01 14:19:50 +0100

    simple node app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-
+const port = 5000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen()
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
