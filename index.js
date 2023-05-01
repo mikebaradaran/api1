426a83802c77bded4bfcf6d12bd3f3855ddae658
Bob <mike.baradaran@qa.com>
2023-05-01 15:53:45 +0100

    better rest format

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var customers = require('./customers.json')
 var orders = require('./orders.json')
-var details = require('./orderDetails.json')
 
 var writeResponse = function(res, str) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -10,10 +9,9 @@ var writeJsonResponse = function(res, str) {
     writeResponse(res, JSON.stringify(str));
 }
 
-apis = "/customerByID/\n/customers\n/orders\n/ordersByCustomerid/\n/orders\n/orderbyCustomerid/"
 var writeHelp = function(res) {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end(apis);
+    res.end("customers\n/orders");
 }
 
 var notFound = function(res) {
@@ -24,35 +22,34 @@ var notFound = function(res) {
 const server = require('http').createServer();
 
 server.on('request', (req, res) => {
-
     res.setHeader("Access-Control-Allow-Origin", "*");
 
     var urlParts = req.url.toLowerCase().split('/');
     var command = urlParts[1];
     var id = urlParts[2];
 
-    if (command == 'customerbyid') {
-        var cus = customers.filter(c => c.CustomerID.toLowerCase() == id);
-        if (cus.length != 0)
-            writeJsonResponse(res, cus[0]);
+    if (command == 'customers') {
+        if(id==null)
+          writeJsonResponse(res, customers);
         else
+        {
+          var cus = customers.filter(c => c.CustomerID.toLowerCase() == id);
+          if (cus.length == 0)
             notFound(res);
-    } else if (command == 'customers') {
-        writeJsonResponse(res, customers);
-    } else if (command == 'ordersbycustomerid') {
-        var ords = orders.filter(o => o.CustomerID.toLowerCase() == id);
-        writeJsonResponse(res, ords);
-    } else if (command == 'orders') {
-        writeJsonResponse(res, orders);
-    } else if (command == 'orderbycustomerid') {
-        var data = details.filter(o => o.OrderID == id);
-        writeJsonResponse(res, data);
-    } else if (command == 'getorderdetails') {
-        writeJsonResponse(res, details);
-    } else if (command == 'customersbycity') {
-        var data = customers.filter(c => c.City.toLowerCase() == id);
-        writeJsonResponse(res, data);
-    } else {
+          else
+            writeJsonResponse(res, cus[0]);
+        }
+      }        
+    else if (command == 'orders') {
+        if(id==null)
+          writeJsonResponse(res, orders);
+        else {
+          console.log('getting orders with id=' + id);
+          var data = orders.filter(o => o.CustomerID.toLowerCase() == id);
+          writeJsonResponse(res, data);
+        }
+      }
+    else {
         writeHelp(res);
     }
 });
